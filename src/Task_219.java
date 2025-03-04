dc756f665732336052af13414fb19f9a68731a98
6yp4uk <andreiburmagin@gmail.com>
2025-03-04 06:43:17 +0300

    commit to github

--- src/Task_219.java
+++ src/Task_219.java
@@ -1,25 +1,25 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 public class Task_219 {
         public static void main (String[] args){
-            Map<String,Integer> int_map = new HashMap<String,Integer>();
             System.out.println("Введите 3 числа");
             Scanner in = new Scanner(System.in);
             int a = in.nextInt();
-            int_map.put("a", a);
             int b = in.nextInt();
-            int_map.put("b", b);
             int c = in.nextInt();
-            int_map.put("c", c);
-            for (i = 0; i < int_array.length-1; i++){
-                int sum;
-                if (int_array[i] != 0  && int_array[i] % 5 == 0){
-                    System.out.println()
-                }
-            }
-            System.out.println(int_array.length);
+            Map<String,Integer> abc = new HashMap<String,Integer>();
+            if (a != 0 && a % 5 == 0 ) abc.put("a", a);//System.out.print("a:" + a);
+            if (b != 0 && b % 5 == 0 ) abc.put("b", b);//System.out.print("b:" + b);
+            if (c != 0 && c % 5 == 0 ) abc.put("c", c);//System.out.print("c:" + c);
+            String string_abc = abc.toString().replace("{","").replace("}","");
+            System.out.print(string_abc);
+            //for(Map.Entry<String, Integer> items : abc.entrySet()){
+               //System.out.printf("%s:%s, ", items.getKey(),items.getValue());
+                        //}
+            if ((((a % 5 ) != 0) && ((b % 5) !=0) && ((c % 5) !=0)) ||
+                    (a == 0 && b == 0 && c == 0)) System.out.println("нет значений, кратных 5");
+           // if (a == 0 && b == 0 && c == 0) System.out.println("!!!!!");
 
         }
-
-    }
+}
