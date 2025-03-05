ad95d791ebb36abd74ec494045eafe6ce3bfd1b1
6yp4uk <andreiburmagin@gmail.com>
2025-03-05 20:28:20 +0300

    change Task 218

--- src/Task_218.java
+++ src/Task_218.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class Task_218 {
     public static void main (String[] args){
         System.out.println("Как тебя зовут?");
-        System.out.print("Введи свое имя: ");
+        //System.out.print("Введи свое имя: ");
         Scanner in = new Scanner(System.in);
         String name = in.nextLine();
-        System.out.println("Привет, " + name + "!");
+        System.out.printf("Привет, %s!",name);
 
     }
 
